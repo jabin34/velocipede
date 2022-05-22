ae400c79ee822f5bc0b880e07bf6cc9100744b82
jabin34 <isratjabinesha@gmail.com>
2022-05-23 01:02:48 +0900

    Server connection with mongo atlas done

--- cycle-server/index.js
+++ cycle-server/index.js
@@ -3,10 +3,35 @@ const cors = require('cors');
 const app = express(); 
 const port = process.env.PORT || 4000 ;
 require('dotenv').config();
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 //middleware 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vycem.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("cycle").collection("tools");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run(){
+   try{
+    await  client.connect();
+    const toolsCollection = client.db("cycle").collection("tools");
+    
+    console.log("cycle db connected");
+   }finally{
+
+   }
+}
+run().catch(console.dir);
+
+
+
 
 
 app.get('/',(req,res)=>{
